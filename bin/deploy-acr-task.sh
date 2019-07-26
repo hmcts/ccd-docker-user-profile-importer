5f29868dcc305d20a6e689bc2620b46c709c00dd
markry-kainos <43344584+markry-kainos@users.noreply.github.com>
2019-07-26 14:13:20 +0100

    Use GitHub token in vault for deploying ACR task (#3)

--- bin/deploy-acr-task.sh
+++ bin/deploy-acr-task.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+GITHUB_TOKEN=$(az keyvault secret show --vault-name infra-vault-prod --name hmcts-github-apikey -o tsv --query value)
+
 az acr task create \
     --registry hmctspublic \
     --name task-ccd-user-profile-importer \
